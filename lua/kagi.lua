7b70707b556c1d003d4af50797f0a51615083a66
Stefan Rakel <stefan.rakel@de.bosch.com>
2024-08-27 14:53:18 +0200

    Make Kagi also work for current line if nothing is selected and don't request if nothing is selected

--- lua/kagi.lua
+++ lua/kagi.lua
@@ -7,7 +7,15 @@ function M.replaceSelection()
   local buffer = vim.api.nvim_get_current_buf()
   local startline = vim.fn.getpos("'<")[2] - 1
   local endline = vim.fn.getpos("'>")[2]
+  local currentMode = vim.api.nvim_get_mode().mode
+  if currentMode ~= "v" and currentMode ~= "V" then
+    endline = vim.fn.getpos(".")[2]
+    startline = endline - 1
+  end
   local selectedText = table.concat(vim.api.nvim_buf_get_lines(buffer, startline, endline, false), " ")
+  if selectedText == "" then
+    return
+  end
   req.perform(selectedText, function(result)
     --Extract code
     local newText = extract.code(result.data.output)
